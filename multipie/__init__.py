2d1c4c1c7da330648a8cb2e706133963a274e3d8
HK <hk@meiji.ac.jp>
2024-08-08 23:21:39 +0900

    update

--- multipie/__init__.py
+++ multipie/__init__.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-__version__ = "1.2.2"
+__version__ = "1.2.3"
 __top_dir__ = os.path.normpath(os.path.dirname(__file__) + "/../") + "/"
 __bin_dir__ = __top_dir__ + "multipie/binary_data/"
 
