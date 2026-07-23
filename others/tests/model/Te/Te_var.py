a662ed1399cec5988499eda580d6373ec3ae826a
RO <65672632+rikuto-oiwa-phys@users.noreply.github.com>
2026-07-24 00:25:37 +0900

    add Te case

---
+++ others/tests/model/Te/Te_var.py
@@ -0,0 +1,7 @@
+"""
+Correspondence between zj and atomic variable.
+- correspondence for each bond cluster, dict[bond_name, dict[zj, expression in terms of atomic variables] ].
+- only for SAMB with identity irrep.
+"""
+
+Te_var = {"A;A_001_1": {}}
---
+++ others/tests/model/Te/Te_z.py
@@ -0,0 +1,6 @@
+"""
+Parameter dict.
+- finite parameter, dict[zj, value].
+"""
+
+Te_z = {"z4": 0.0, "z6": 1.0, "z8": 0.2}
